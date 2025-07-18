a60ae411dfc5be8654404e9b00ef9fdbef266e97
Tejasri Elabotharam <tejasrielabotharam@Mac.fritz.box>
2025-07-18 21:14:14 +0200

    Set Python version

--- venv311/bin/activate.fish
+++ venv311/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/tejasrielabotharam/Downloads/sentiment_analysis_pipeline/venv311
+set -gx VIRTUAL_ENV /Users/tejasrielabotharam/Downloads/tweets/venv311
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
